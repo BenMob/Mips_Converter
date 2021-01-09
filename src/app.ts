ae9fb119c08f0742e02b72f7c1b7af3589662765
Benjamin <omben001@cougars.csusm.edu>
2021-01-09 13:04:12 -0800

    Adds express

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,12 @@
-import Instruction from "./classes/Instruction"
+import express from "express";
 
-window.onload = () => {
-   const input: HTMLInputElement = <HTMLInputElement>document.querySelector("#input");
-   const submit: Element = <HTMLElement>document.querySelector("#submit");
-   const error: HTMLElement = <HTMLElement>document.querySelector("#error");
-   const format: HTMLElement = <HTMLElement>document.querySelector("#format");
-   const instruction: HTMLElement = <HTMLElement>document.querySelector("#instruction");
-   const decimal: HTMLElement = <HTMLElement>document.querySelector("#decimal");
-   const binary: HTMLElement = <HTMLElement>document.querySelector("#binary");
+const app = express();
+const port = 8080;
 
-   if(submit){
-      submit.addEventListener("click", () => {
-         if(input.value){
-            error.innerHTML = " ";
-            instruction.innerHTML = `Instruction: ${input.value}`;
-            format.innerHTML = "Format: R"
-            decimal.innerHTML = "Decimal: 34";
-            binary.innerHTML = "Binary: 0000 1232 00102 00023 0230";
+app.get("/", (req : express.Request, res: express.Response) => {
+   res.send("Express App Set up with Typescript");
+})
 
-         }else{
-            console.log("Problem")
-            error.innerHTML = " ";
-            error.innerHTML = "Please, type something!"
-         }
-
-         //if(Instruction.correct(input?.nodeValue))
-      })
-   }else{
-      console.error("There is no id called 'submit'")
-   }
-
-
-}
+app.listen(port, () => {
+   console.log(`Server started on http://localhost:8080`)
+});
