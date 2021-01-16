183ec48f75ea9e6c95a9bdd1d465d9b06038a664
Benjamin <omben001@cougars.csusm.edu>
2021-01-15 20:39:42 -0800

    Adds custom error messages for Instruction class

--- src/errors/Instruction_e.ts
+++ src/errors/Instruction_e.ts
@@ -7,6 +7,14 @@ class ErrorMessages {
     static unSupportedOrInvalidCommand(command: string): string{
         return `${command} is invalid or unsupported!`;
     }
+
+    static unSuppotedOrInvalidRegister(register: string): string{
+        return `${register} is invalid or unsupported!`
+    }
+
+    static invalidImmediate(immediate: string): string{
+        return `${immediate} is in invalid value for an immediate or an address!`
+    }
 }
 
 export default ErrorMessages;
