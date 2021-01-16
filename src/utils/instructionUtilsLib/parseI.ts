1ba87f7a27ceb662d54bcbf496cfde97ec5d4e8b
Benjamin <omben001@cougars.csusm.edu>
2021-01-15 23:46:45 -0800

    Adds algorithm that takes care of mips instructions SW amd LW

--- src/utils/instructionUtilsLib/parseI.ts
+++ src/utils/instructionUtilsLib/parseI.ts
@@ -12,16 +12,52 @@
  */
 
  function parseI(instructionArray: string[]): IParameters | null{
-    const expectedLength = 4;
+
+    // Extract offset and source register from expression if tje instructionArray is of LW or SW 
+    const peelOffset = (expression: string): {offset: string, reg: string} | null =>{
+        const openingParenthesis = expression.indexOf('(');
+        const closingParenthesis = expression.lastIndexOf(')');
+        try {
+            if(openingParenthesis && closingParenthesis){
+                const offset = expression.substring(0, openingParenthesis);
+                const reg = expression.substring(openingParenthesis + 1, closingParenthesis)
+                return { offset, reg };
+            }else {
+                throw new Error(`Invalid expression ${expression}`)
+            }
+        } catch (error) {
+            console.log(error);
+            return null;
+        }
+    }   
+
+
+    const expectedLengthByDefault = 4;
+    const expectedLengthForLWandSW = 3
     try {
-        if(instructionArray.length === expectedLength){
+        if(instructionArray.length === expectedLengthByDefault){
             return {
                 param1: instructionArray[0],
                 param2: instructionArray[1],
                 param3: instructionArray[2],
-                param4: instructionArray[3]
+                param4: instructionArray[expectedLengthByDefault - 1]
             }
-        }else throw new Error (`Invalid instruction array ${instructionArray}`)
+        }else if(instructionArray.length === expectedLengthForLWandSW){
+            const parsedExpression = peelOffset(instructionArray[expectedLengthForLWandSW - 1]);
+     
+            if(parsedExpression){
+                return {
+                   param1: instructionArray[0],
+                   param2: instructionArray[1],
+                   param3: parsedExpression.reg,
+                   param4: parsedExpression.offset
+                }
+            }else {
+                throw new Error(`Invalid expression `);
+            }
+        }else{
+            throw new Error (`Invalid instruction array ${instructionArray}`)
+        }
     } catch (error) {
         console.log(error);
         return null;
