65ca84965511bbff4d93529f476c93eecdec9622
Benjamin <omben001@cougars.csusm.edu>
2021-01-16 23:42:28 -0800

    Adds instruction controller logic

--- src/controllers/converterController.ts
+++ src/controllers/converterController.ts
@@ -1,16 +1,18 @@
 import Instruction from "../models/Instruction"
 import express from "express";
-import operations from "../data/operations.json";
+
 
 export const mipsToMachine = (req: express.Request, res: express.Response) => {
-
     const instruction = new Instruction(req.body.instruction);
-    const error = instruction.getErrorMessage();
-    
-    if(error){
-        res.json(error);
-    }else{
-        res.json(instruction);
-    }
+
+    instruction.init().then(() => {
+        const error = instruction.getErrorMessage();
+        console.warn(error);
+        if(error){
+            res.json({error: error});
+        }else{
+            res.json(instruction.getType());
+        }
+    })
 }
 
