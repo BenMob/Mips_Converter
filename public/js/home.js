4a42585f7a83c775907693a48c4715aece4b7da3
Benjamin <omben001@cougars.csusm.edu>
2021-01-16 23:56:33 -0800

    Adds logic to submit mips instructions to the server

--- public/js/home.js
+++ public/js/home.js
@@ -1,3 +1,35 @@
 window.onload = function(){
-    console.log("Home Page");
+    const post = (url = "", data = {}) => {
+        fetch(url, {
+            method: "POST",
+            mode: "cors",
+            cache: "no-cache",
+            credentials: "same-origin",
+            headers: {
+               "Content-Type": "application/json"
+            },
+            redirect: "follow",
+            referrerPolicy: "no-referrer",
+            body: JSON.stringify(data)
+        }).then((response) => {
+            return response.json();
+        }).then((data) => {
+            console.log(data);
+        }).catch(err => {
+            console.warn(err)
+        })
+    }
+
+    const submit = () => {
+        const instruction = document.querySelector("#input").value;
+        const url = "http://localhost:8080/convert";
+        post(url, {instruction});
+    }
+
+    const form = document.querySelector("#form");
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        submit();
+    })
+
 }
