4ece6a1b6d5c08b3b8430f0f239fd1888a631df3
Benjamin <omben001@cougars.csusm.edu>
2021-01-17 23:00:37 -0800

    exports two new utility mehods

--- src/utils/instructionUtils.ts
+++ src/utils/instructionUtils.ts
@@ -3,9 +3,13 @@
 import parseR from "./instructionUtilsLib/parseR";
 import parseI from "./instructionUtilsLib/parseI";
 import parseJ from "./instructionUtilsLib/parseJ";
+import assemblyToDecimal from "./instructionUtilsLib/assemblyToDecimal";
+import decimalToBinary from "./instructionUtilsLib/decimalToBinary";
 
 export {
     parseR,
     parseI,
-    parseJ
+    parseJ,
+    assemblyToDecimal,
+    decimalToBinary
 }
