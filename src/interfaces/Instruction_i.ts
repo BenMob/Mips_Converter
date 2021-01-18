36124d3695d28457abae8f7e5ae559b835b569b5
Benjamin <omben001@cougars.csusm.edu>
2021-01-17 23:01:41 -0800

    update types for decimal and binary attributes

--- src/interfaces/Instruction_i.ts
+++ src/interfaces/Instruction_i.ts
@@ -1,12 +1,14 @@
 import { Itype_i } from "./Itype_i";
 import { Jtype_i } from "./Jtype_i";
 import { Rtype_i } from "./Rtype_i";
+import { DecimalSchema } from "../utils/instructionUtilsLib/assemblyToDecimal";
+import { BinarySchema } from "../utils/instructionUtilsLib/decimalToBinary";
 
 export interface Instruction_i{
     getType(): Itype_i | Jtype_i | Rtype_i | undefined
     getAssembly(): string | undefined;
-    getDecimal(): string | undefined;
-    getBinary(): string | undefined;
+    getDecimal(): DecimalSchema | undefined;
+    getBinary(): BinarySchema | undefined;
     getErrorMessage(): string | undefined;
 
     /*****  Implementation Details *****/
