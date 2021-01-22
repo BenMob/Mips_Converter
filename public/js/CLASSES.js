9bb4c3d9397872d8576264473f5b3ddd69e0081a
Benjamin <omben001@cougars.csusm.edu>
2021-01-22 01:06:36 -0800

    Adds Tailwind classes used in UTILS.js

---
+++ public/js/CLASSES.js
@@ -0,0 +1,12 @@
+/**
+ * CSS Classes for dom elements used in UTILS.js
+ */
+
+ class CLASSES{
+
+     // Table 
+     static tableTopLeftFieldClasses = ["pr-2", "w3-animate-opacity"];
+     static tableHeaderRowFieldClasses = ["p-1", "font-bold", "border-b", "border-gray-400", "w3-animate-opacity"];
+     static tableHeaderColFieldClasses = ["pl-2", "pr-2", "font-bold", "border-r", "border-gray-400", "w3-animate-opacity"];
+     static tableBodyRowFieldClasses = ["p-1", "w3-animate-opacity"];
+ }
