12667457ee7ece65e51bb5c5914e7a606375812e
Benjamin <omben001@cougars.csusm.edu>
2021-01-22 01:16:46 -0800

    Adds dom yulity methods

---
+++ public/js/DOM.js
@@ -0,0 +1,51 @@
+/**
+ * A Class containing static functions to help create various 
+ * dom elements.
+ */
+class DOM{
+    static getDiv(){
+        return document.createElement('div')
+    }
+
+    static getP(){
+        return document.createElement('p')
+    }
+
+    static getSpan(){
+        return document.createElement('span')
+    }
+
+    static getTable(){
+        return document.createElement('table')
+    }
+
+    static getTr(){
+        return document.createElement('tr')
+    }
+
+    static getTd(){
+        return document.createElement('td')
+    }
+
+    static addClass(node, classNames){
+        if(classNames.length > 0){
+            classNames.forEach(className => {
+                if(!node.classList.contains(className)){
+                    node.classList.add(className);
+                }
+            });
+        }
+    }
+
+    static removeClass(node, classNames){
+        classNames.forEach(className => {
+            if(node.classList.contains(className)){
+                node.classList.remove(className);
+            }
+        })
+    }
+    
+    static write(node, content){
+        node.innerHTML = content;
+    }
+}
