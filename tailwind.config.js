03b826f2775b55f864edc3b98b43b80632ff33dd
Benjamin <omben001@cougars.csusm.edu>
2021-01-22 20:10:26 -0800

    Config tailwind to reduce CSS file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: true,
+    content: [
+    './src/**/*.ejs',
+    './public/**/*.js'
+    ]
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
